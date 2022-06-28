35a57b492b3e1f1e3832dab7d54dd748909d95cd
Dorwin Liang <liangdorwin@gmail.com>
2022-06-27 22:19:39 -0400

    js

--- script.js
+++ script.js
@@ -38,5 +38,54 @@ function operate(operator, a, b) {
     }
 
 }
+let keypadNumber = document.querySelectorAll(".keypad-number");
+let number = document.getElementById('number');
+let operators = document.querySelectorAll(".keypad-right");
+let clear = document.getElementById('clear-button');
+let displayValue;
+let secondValue;
+let operatorSign;
+//display number pressed and cap number to 14
+function displayNumber() {
+    if (number.textContent.length > 13) {
+        return;
+    };
+    
+    if (number.textContent.includes(".")) {
+        if (this.textContent === ".")  {
+            return;
+        }
+    };
+    
+    console.log(typeof number.textContent);
+    
+    number.textContent += this.textContent; // add the number pressed
+    displayValue = number.textContent; //store number into another variable
+}
 
-console.log(operate('divide', 2,3));
+function clearDisplay() {
+    displayValue = number.textContent;
+    number.textContent = "";
+    console.log(displayValue);
+}
+
+function displaySign() {
+    if (operatorSign) {  //if user already pressed a operator sign, break out of function
+        return;
+    }
+    operatorSign = this.id;
+    console.log(operatorSign);
+
+}
+
+keypadNumber.forEach(num => {
+    num.addEventListener('click', displayNumber);
+});
+
+clear.addEventListener('click',clearDisplay);
+
+operators.forEach(sign => {
+    sign.addEventListener('click', displaySign)
+});
+
+
